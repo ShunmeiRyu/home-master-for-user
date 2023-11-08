a6ef5ff9ee1896c31d8f190ef5c4c5ca8fbab957
LiXintao <xintao_li@gotask.co.jp>
2023-11-08 11:02:36 +0900

    fix: eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,45 +3,45 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
     },
     colors: {
-      "gun-powder": {
-        100: "#423E5B",
-        90: "#55516B",
-        80: "#68657C",
-        60: "#8E8B9D",
-        40: "#B3B2BD",
-        20: "#D9D8DE",
-        10: "#ECECEF",
+      'gun-powder': {
+        100: '#423E5B',
+        90: '#55516B',
+        80: '#68657C',
+        60: '#8E8B9D',
+        40: '#B3B2BD',
+        20: '#D9D8DE',
+        10: '#ECECEF'
       },
-      "sunglow": {
-        100: "#FFBD34",
-        90: "#FFC448",
-        80: "#FFCA5D",
-        60: "#FFD785",
-        40: "#FFE5AE",
-        20: "#FFF2D6",
-        10: "#FFF8E8",
+      sunglow: {
+        100: '#FFBD34',
+        90: '#FFC448',
+        80: '#FFCA5D',
+        60: '#FFD785',
+        40: '#FFE5AE',
+        20: '#FFF2D6',
+        10: '#FFF8E8'
       },
-      "shark": {
-        100: "#1F2128",
-        90: "#35373E",
-        80: "#4C4D53",
-        60: "#797A7E",
-        40: "#A5A6A9",
-        20: "#D2D3D4",
-        10: "#E9E9EA",
-      },
+      shark: {
+        100: '#1F2128',
+        90: '#35373E',
+        80: '#4C4D53',
+        60: '#797A7E',
+        40: '#A5A6A9',
+        20: '#D2D3D4',
+        10: '#E9E9EA'
+      }
     }
   },
-  plugins: [],
+  plugins: []
 }
