7c97d32db958de83f9974ee7a31d4fef060c9843
oushunkyoku <chunxu_wang@gotask.co.jp>
2023-11-08 18:07:21 +0900

    email-pwd-input,header-footer-simple

---
+++ app/auth/page.js
@@ -0,0 +1,11 @@
+import EmailInput from "../components/common/EmailInput";
+import PwdInput from "../components/common/PwdInput";
+
+export default function Login() {
+    return (
+        <div>
+            <EmailInput />
+            <PwdInput />
+        </div>
+    )
+}
