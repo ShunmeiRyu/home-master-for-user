69d3ed918961568319c46c9cceee83024e51c130
LiXintao <xintao_li@gotask.co.jp>
2023-11-09 18:43:05 +0900

    feature: button&&buttonLoading&&errorAlert

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,27 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['standard', 'plugin:react/recommended'],
+  extends: ["standard", "plugin:react/recommended"],
   overrides: [
     {
       env: {
         node: true
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: "script"
       }
     }
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'space-before-function-paren': 'off'
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "space-before-function-paren": "off",
+    quotes: ["error", "double"]
   }
 }
