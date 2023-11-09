702d4f832ae8343948f01faa4a535bccf4b95423
oushunkyoku <chunxu_wang@gotask.co.jp>
2023-11-09 11:47:18 +0900

    cache

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>HOME</div>
+    <div>
+      <div>HOME</div>
+      <Link href="/login">LOGIN</Link>
+      <span>       </span>
+      <Link href="/login/register">REGISTER</Link>
+    </div>
   )
 }
