1566cc41efd1036facfe871745eca7c6e56896aa
oushunkyoku <chunxu_wang@gotask.co.jp>
2023-11-14 11:31:20 +0900

    verifyemail:verifycode

--- app/(auth)/login/register/verifyEmail/page.js
+++ app/(auth)/login/register/verifyEmail/page.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import VerifyCode from "../../../../components/common/VerifyCode";
 
 export default function VerifyEmail() {
     return (
         <div className="flex flex-col">
             <p className="mb-5 tracking-wider font-bold text-xl">メール検証</p>
             <p className="font-sans text-xs font-normal mb-5">6桁の確認コードをdemo@gotask.co.jp宛にメールしましたので、下のボックスにコードを入力してメールを確認してください。</p>
-            <p className="font-sans text-xs font-normal mb-5 self-center">コードをお持ちでない場合は、<Link  href="/login" className="underline">コード再送信</Link></p>
+            <VerifyCode />
+            <p className="font-sans text-xs font-normal mb-5 self-center mt-10">コードをお持ちでない場合は、<Link href="/login" className="underline">コード再送信</Link></p>
             <Link href="/login" className="font-sans text-xs font-normal mt-3 underline self-center">すでに会員の方はログイン</Link>
         </div>
     )
